282b5538f9dd5494d4926beb4f6b73508b00a19f
barretot <barretoruan77@gmail.com>
2021-06-17 03:57:54 -0300

    fix

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,29 +1,27 @@
 import authConfig from '../../config/authConfig';
 
 import jwt from 'jsonwebtoken';
-import { Request, Response } from 'express';
+import { Response } from 'express';
 import { promisify } from 'util';
 
-export default async function (
-  request: Request,
-  response: Response,
-  next,
-): Promise<Response> {
+export default async function (request, response, next): Promise<Response> {
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
     return response.status(401).json({ error: 'Token não fornecido' });
   }
 
-  if (Date.now() >= authConfig.expiresIn) {
+  if (Date.now() >= Object(authConfig.expiresIn)) {
     return response.status(401).json({ error: 'Sessão inválida' });
   }
 
   const [, token] = authHeader.split(' ');
 
   try {
-    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
-
+    const decoded: jwt.SigningKeyCallback = await promisify(jwt.verify)(
+      token,
+      authConfig.secret,
+    );
     request.userId = decoded.id;
 
     return next();
